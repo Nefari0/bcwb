3f67301f6092c57c4e5d48b655d3b6cf40d22677
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-07 20:37:46 -0600

    styling

--- bcwb/src/Components/Nav/Nav.jsx
+++ bcwb/src/Components/Nav/Nav.jsx
@@ -1,6 +1,10 @@
 import './Nav.css'
+import Hamburgar from './Hamburger'
+import { useState } from 'react'
 
 const Nav = (props) => {
+
+    const [ menu,setMenu ] = useState(false)
 
     return(
         <nav>
@@ -10,12 +14,17 @@ const Nav = (props) => {
                 <li>item</li>
                 <li>item</li>
             </ul>
-
-            <ul className='mobile-nav' >
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
+            <Hamburgar setMenu={setMenu} menu={menu} /> 
+            <h1>logo</h1>
+            <h1>search</h1>
+            <ul className={`mobile-nav ${menu ? true : 'closed'}`} >
+                {menu ? <div>
+                    <li>item</li>
+                    <li>item</li>
+                    <li>item</li>
+                    <li>item</li>
+                    <p onClick={() => setMenu(false)} >X</p>
+                </div> : null}
             </ul>
         </nav>
     )
