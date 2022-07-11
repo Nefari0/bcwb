d3682a581a0a487ac292fa752f3f528339e1299d
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-11 08:37:09 -0600

    This component will be used to display all photos, album, and their corrosponding information. Adding and editing options will also be accessable here

---
+++ bcwb/src/Components/Admin/Photos/PhotoDisplay.jsx
@@ -0,0 +1,35 @@
+import { useState,useEffect } from "react"
+
+
+const PhotoDisplay = (props) => {
+
+    // Get all photos
+    function getPhotos() {
+
+    }
+
+    function getAlbums() {
+
+    }
+
+    function addPhoto() {
+        // what album? // optional
+
+        // what title
+    }
+
+
+    // function inputHandler(prop,val) {
+    //     setState
+    // }
+
+
+
+
+
+    return(<div>
+
+    </div>)
+}
+
+export default PhotoDisplay
