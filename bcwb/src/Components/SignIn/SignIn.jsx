295def79c995e94d900c8d5e3860e537f0f78a29
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-11 08:12:44 -0600

    firebase/auth from .base.js

--- bcwb/src/Components/SignIn/SignIn.jsx
+++ bcwb/src/Components/SignIn/SignIn.jsx
@@ -1,18 +1,17 @@
-import { signInWithGooglePopup,createUserDocumentFromAuth } from "../../Utils/firebase/firebase.utils"
+import { signInWithGooglePopup,createUserDocumentFromAuth } from "../../base"
 
 const SignIn = (props) => {
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
-        console.log(user)
     }
 
     return(
-        <div style={{height:'400px',width:'400px',backgroundColor:'blue'}} >
+        <form>
             <h2 style={{color:'#555'}} >sign in</h2>
             <button onClick={() => logGoogleUser()}>sign in with google popup</button>
-        </div>
+        </form>
     )
 }
 
