7d46ebbe82325357f79ca43a9deb5f90b536f3b8
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-12 16:40:44 -0600

    Admin page

---
+++ bcwb/src/Components/Admin/Admin.jsx
@@ -0,0 +1,13 @@
+import './Admin.scss'
+import CreateRecipe from './CreateRecipe/CreateRecipe'
+
+const Admin = () => {
+    
+    return(
+        <main className="admin">
+            <CreateRecipe />
+        </main>
+    )
+}
+
+export default Admin
