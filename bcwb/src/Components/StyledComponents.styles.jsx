7bbd4d1dff69d678168d95c8c2de6424f94cb047
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-13 16:40:20 -0600

    styled components

---
+++ bcwb/src/Components/StyledComponents.styles.jsx
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components';
+
+export const Error = styled.div`
+  position: absolute;
+  width:500px;
+  background-color:grey;
+  z-index:1;
+  border-radius:5px;
+  box-shadow: 0px 5px 20px -7px #000000;
+`;
+
+export const PortraitImage = styled.div`
+  height:400px;
+  width:267px;
+  z-index:1;
+  overflow:hidden;
+  border-radius:5px;
+  margin:auto;
+`;
+
+export const LandscapeImage = styled.div`
+  height:267px;
+  width:400px;
+  z-index:1;
+  overflow:hidden;
+  border-radius:5px;
+  margin:auto;
+`
+export const ThumbnailImage = styled.div`
+  height:50px;
+  width:50px;
+  border-radius:50%;
+`
