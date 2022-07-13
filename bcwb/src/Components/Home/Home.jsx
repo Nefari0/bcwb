a233e104f57045a2e30f0ac171a0cf1efb69ac22
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-13 12:03:04 -0600

    cleaning

--- bcwb/src/Components/Home/Home.jsx
+++ bcwb/src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
-import './Home.css'
-import axios from 'axios'
-import { useState,useEffect } from 'react'
+import './Home.scss'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import LatestRecipes from './LatestRecipes/LatestRecipes'
 import FindRecipes from './FindRecipes/FindRecipes'
 
@@ -8,18 +8,18 @@ const Home = () => {
 
     const [item,setItem] = useState([])
 
-    useEffect(() => {
-        axios.get('/api/photos/get').then(res => {
-            setItem(res.data)
-        })
-    })
-
     return(
         <main className='home'>
             <h1>The Latest Recipes</h1>
             <LatestRecipes />
             <h1>Find Recipes For...</h1>
             <FindRecipes />
+            {/* <Photos /> */}
+            {/* <Pinterest /> */}
+            <footer>
+                <Link to="/admin" ><h1>admin</h1></Link>
+                <h1>footer</h1>
+            </footer>
         </main>
     )
 }
