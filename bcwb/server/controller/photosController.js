a4f2080516ded7f9a43c270e78b7bd6a1309c654
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-15 04:43:36 -0600

    Update and delete photo

--- bcwb/server/controller/photosController.js
+++ bcwb/server/controller/photosController.js
@@ -11,5 +11,19 @@ module.exports = {
         const { url,title,album } = req.body
         const photo = await db.photos.add_photo([url,title,album])
         return res.status(200).send(photo)
+    },
+
+    deletePhoto: async (req,res) => {
+        const db = req.app.get('db')
+        const { photo_id } = req.params
+        const photo = await db.photos.delete_photo([photo_id])
+        return res.status(200).send(photo)
+    },
+
+    updatePhoto: async (req,res) => {
+        const db = req.app.get('db')
+        const { url,album,title,photo_id } = req.body
+        const photo = await db.photos.update_photo([url,title,album,photo_id])
+        return res.status(200).send(photo)
     }
 }
