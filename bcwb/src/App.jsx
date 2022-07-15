ac4690dce2754d40cac02f56ff4f798276f78854
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-14 20:28:06 -0600

    Styling and cleaning code. Added list styling components

--- bcwb/src/App.jsx
+++ bcwb/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
 import Nav from './Components/Nav/Nav';
-import Home from './Components/Home/Home';
 import routes from './routes';
 import { Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <Link to='/' ><img className='hero' /></Link>
         <Nav />
-      </header>
+        </header>
         {routes}
     </div>
   );
