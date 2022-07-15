1285bb8e1ffc59f4280746324aff1be4bc5bfcb5
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-14 20:32:21 -0600

    Route to Recipe component

--- bcwb/src/routes.js
+++ bcwb/src/routes.js
@@ -1,10 +1,12 @@
 import { Switch,Routes,Route,BrowserRouter as Router } from 'react-router-dom'
 import Admin from './Components/Admin/Admin'
 import Home from './Components/Home/Home'
+import Recipe from './Components/Recipe/Recipe'
 
 export default (
     <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/admin" component={Admin} />
+        <Route path="/recipe/:recipe_id" component={Recipe} />
     </Switch>
 )
