7d93cacd769e2059717f03ef1378b489ecd0130a
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-16 05:23:42 -0600

    Adding delete / change image functionality

--- bcwb/src/Components/Recipe/Instructions/Head/InstructionHead.component.jsx
+++ bcwb/src/Components/Recipe/Instructions/Head/InstructionHead.component.jsx
@@ -1,6 +1,12 @@
 import { InstructionHeader,MainImage,Row,Keys } from "./InstructionHead.styles"
+import { useState } from "react"
+import DeletePhoto from "../../../Admin/Photos/DeletePhoto"
 
 export const InstructionHead = (props) => {
+
+    // --- FOR TESING ONLY --- //
+    const [ isAdmin,setIsAdmin ] = useState(false)
+    // ----------------------- //
 
     const { cover_image_url,title } = props.items[0]
 
@@ -9,6 +15,7 @@ export const InstructionHead = (props) => {
             <MainImage>
                 <img src={cover_image_url} />
             </MainImage>
+            {!isAdmin ? <DeletePhoto url={cover_image_url} /> : null}
 
             <h2 style={{marginTop:'100px',color:'#fff',marginBottom:'0px'}}>{title}</h2>
 
