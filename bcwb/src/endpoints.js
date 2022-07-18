9239bc452b7d3b9565b0b3ab27a8b46effa2a1ab
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-18 07:56:12 -0600

    Stores all endpoints for global access

---
+++ bcwb/src/endpoints.js
@@ -0,0 +1,11 @@
+
+export const RECIPES = {
+    // -- Instruction End Points -- //
+    POST_INSTRUCTION:'/api/instructions/add',
+    DELETE_INSTRUCTION:'/api/instructions/delete/',
+    PUT_INSTRUCTION:'/api/instructions/put',
+    // -- Ingredient End Points -- //
+    POST_INGREDIENT:'/api/ingredient/new',
+    PUT_INGREDIENT:'/api/ingredient/put',
+    DELETE_INGREDIENT:'/api/ingredient/delete/'
+}
