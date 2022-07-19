8baa99d55caa9f111a83d747d8034990ed387612
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-19 17:53:08 -0600

    Edit recipe and get photo by url

--- bcwb/src/endpoints.js
+++ bcwb/src/endpoints.js
@@ -1,5 +1,7 @@
 
 export const RECIPES = {
+    // -- Recipe -- //
+    EDIT_RECIPE:'/api/recipes/edit',
     // -- Instruction End Points -- //
     POST_INSTRUCTION:'/api/instructions/add',
     DELETE_INSTRUCTION:'/api/instructions/delete/',
@@ -8,4 +10,8 @@ export const RECIPES = {
     POST_INGREDIENT:'/api/ingredient/new',
     PUT_INGREDIENT:'/api/ingredient/put',
     DELETE_INGREDIENT:'/api/ingredient/delete/'
+}
+
+export const PHOTOS = {
+    GET_PHOTOS_WITH_URL:'/api/photos/get/url/'
 }
