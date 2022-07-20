952c4ad26fb50fba08873e7df308d15703b2840b
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-19 21:00:45 -0600

    HashRouter

--- bcwb/src/App.jsx
+++ bcwb/src/App.jsx
@@ -2,9 +2,11 @@ import './App.css';
 import Nav from './Components/Nav/Nav';
 import routes from './routes';
 import { Link } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 function App() {
   return (
+    <HashRouter>
     <div>
       <header className="App-header">
         <Link to='/' ><img className='hero' /></Link>
@@ -12,6 +14,7 @@ function App() {
         </header>
         {routes}
     </div>
+    </HashRouter>
   );
 }
 
