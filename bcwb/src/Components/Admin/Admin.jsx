8635dfef1a5c526c9b08ae7b2b04c2d14ca24389
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-19 18:00:38 -0600

    Reset secure access

--- bcwb/src/Components/Admin/Admin.jsx
+++ bcwb/src/Components/Admin/Admin.jsx
@@ -1,12 +1,20 @@
 import './Admin.scss'
 import CreateRecipe from './CreateRecipe/CreateRecipe'
 import { ViewRecipes } from './ViewRecipes/ViewRecipes'
+// import AddPhotos from './Photos/AddPhotos'
+import { useState } from 'react'
 
 const Admin = () => {
+
+    const resetAccess = () => {
+        localStorage.setItem('text','')
+    }
     
     return(
         <main className="admin">
+            <button onClick={resetAccess} >logout admin</button>
             <ViewRecipes />
+            {/* <AddPhotos updateDB={null} label={'add photo'}/> */}
             <CreateRecipe />
         </main>
     )
