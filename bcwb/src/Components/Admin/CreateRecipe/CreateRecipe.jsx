34c307721721c6b5abe0547a1482c4d958489848
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-19 19:12:08 -0600

    Immediately routes user to recipe view, where more features can be added

--- bcwb/src/Components/Admin/CreateRecipe/CreateRecipe.jsx
+++ bcwb/src/Components/Admin/CreateRecipe/CreateRecipe.jsx
@@ -2,10 +2,8 @@ import './CreateRecipe.scss'
 import { useState } from 'react'
 import FormInput from '../../Form/FormInput'
 import Button from '../../Form/Button'
-import AddPhotos from '../Photos/AddPhotos'
 import axios from 'axios'
 import { ErrorMessage } from '../../ErrorMessage/ErrorMessage'
-import { Route } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
 const defaultState = {
@@ -18,41 +16,20 @@ const defaultState = {
 }
 
 const CreateRecipe = (props) => {
-    const [ exists,setExists ] = useState(false)
+
     const [formFields, setFormFields] = useState(defaultState);
-    const { title, description, ingredient, step, category } = formFields;
-    const [ newRecipe,setNewRecipe ] = useState(null)
-    const [ recipe,setRecipe ] = useState(null)
+    const { title, description, category } = formFields;
     const [ error,setError ] = useState(null)
 
     const createRecipe = (event) => {
         event.preventDefault();
         axios.post('/api/recipes/create',formFields).then(res => {
-            setNewRecipe(res.data)
+            props.history.push(`/recipe/${res.data[0].recipe_id}`)
         }).catch(err => {
-            setError('This title is already being used. Please choose another title')
-        })
-        // then the next step should be available by changing value from null
-    }
-
-    const addInstructionStep = () => {
-        // axios.post(number,text,recipe_id)
-    }
-
-    const addIngredient = () => {
-        // axios.post(content,recipe_id)
-    }
-
-    const createPhotoAlbum = () => { // This photo album has to have the same name as the recipe
-        // axios.post(url,title,album)
-    }
-
-    const addRecipePhoto = async (cover_image_url) => {
-        const { description,title,category,published,recipe_id,pintereste_url } = newRecipe[0]
-        await axios.put('/api/recipes/edit',{description,title,category,published,cover_image_url,recipe_id,pintereste_url}).then(res => {
-            props.history.push(`/recipe/${recipe_id}`)
+            setError(err,'This title is already being used. Please choose another title')
         })
     }
+
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -100,7 +77,8 @@ const CreateRecipe = (props) => {
                 <h1>Create Recipe</h1>
             
                 {error ? <ErrorMessage error={error} setError={setError} /> : null}
-                {newRecipe === null ? initRecipe() : <AddPhotos updateDB={addRecipePhoto} />}        
+
+                {initRecipe()}
             
         </main>
      
