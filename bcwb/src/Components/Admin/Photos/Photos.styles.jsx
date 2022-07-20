7c858c10fdc3d8b6e52787e685d35e001952d75e
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-19 19:22:39 -0600

    Local styling. Yields Controller styling for adjusting photo positioning

---
+++ bcwb/src/Components/Admin/Photos/Photos.styles.jsx
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components';
+
+export const PositionController = styled.form`
+  width:50px;
+  height:50px;
+  background-color:green;
+  position:absolute;
+  top:-50px;
+  left:0px;
+  z-index:1;
+  display:flex;
+  flex-wrap:wrap;
+`
