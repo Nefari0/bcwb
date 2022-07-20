2695d4fd7f4810ce47764b0af97e5e3a63dd43e7
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-19 19:48:14 -0600

    Imporeded endpoints

--- bcwb/src/Components/Recipe/Instructions/Body/Instruction.jsx
+++ bcwb/src/Components/Recipe/Instructions/Body/Instruction.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './Instructions.scss'
 
 const Instruction = (props) => {
-    const { recipe,isAdmin,instruction_id,step,content,grabInstructions,deleteItem,deleteInstructionEndPoint,putInstructionEndPoint,putItem  } = props
+    const { isAdmin,instruction_id,step,content,grabInstructions,deleteItem,DELETE_INSTRUCTION,PUT_INSTRUCTION,putItem  } = props
 
     const [ formFields,setFormFields ] = useState({
         content:content,
@@ -23,14 +23,17 @@ const Instruction = (props) => {
         {isAdmin ?
         <form>
             {/* -- Change step -- */}
-            <input style={{width:'15px'}} type='number'  name="step" onChange={(e) => handleChange(e)} value={formFields.step} />
+            <input style={{width:'35px'}} type='number'  name="step" onChange={(e) => handleChange(e)} value={formFields.step} />
 
             {/* -- Edit content -- */}
             <input type='text' name="content" onChange={(e) => handleChange(e)} value={formFields.content} />
 
             
-            <button onClick={(e) => putItem(e,putInstructionEndPoint,formFields,grabInstructions)} >update item</button>
-            <button onClick={(e) => deleteItem(e,deleteInstructionEndPoint,instruction_id,grabInstructions)} >
+            <button onClick={(e) => putItem(e,PUT_INSTRUCTION,formFields,grabInstructions)} >
+                update item
+            </button>
+            
+            <button onClick={(e) => deleteItem(e,DELETE_INSTRUCTION,instruction_id,grabInstructions)} >
                 delete
             </button>
     </form> : null}</li>
