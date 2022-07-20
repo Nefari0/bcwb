00330fef6c8d11b792a5a3e9362cc53b0156bc9b
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-19 19:51:53 -0600

    Added columns to photo DB

--- bcwb/db/photos/photos_seed.sql
+++ bcwb/db/photos/photos_seed.sql
@@ -2,12 +2,16 @@ CREATE TABLE bcwb_photos (
   photo_id SERIAL PRIMARY KEY,
   url TEXT,
   title varchar(250),
-  album varchar(250)
+  album varchar(250),
+  style_left INTEGER,
+  style_top INTEGER,
 )
 
-INSERT INTO bcwb_photos (url,title,album)
+INSERT INTO bcwb_photos (url,title,album,style_left,style_top)
 VALUES (
   'photo/url/',
   'first photo',
-  'first album'
+  'first album',
+  2,
+  1
 )
