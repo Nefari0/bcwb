181befe428b7e21bf41c4d65d7a12f7d3cbee0dc
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-20 23:50:30 -0600

    Re-styling. Adding new features. Not complete, but can update title and add/delete cover image

--- bcwb/src/Components/Recipe/Instructions/Head/InstructionHead.component.jsx
+++ bcwb/src/Components/Recipe/Instructions/Head/InstructionHead.component.jsx
@@ -1,15 +1,19 @@
-import { InstructionHeader,MainImage,Row,Keys } from "./InstructionHead.styles"
+import { InstructionHeader,MainImage,Row } from "./InstructionHead.styles"
+import { DescriptionText } from "../../../StyledComponents.styles"
 import { useState,useEffect } from "react"
 import DeletePhoto from "../../../Admin/Photos/DeletePhoto"
 import AddPhoto from "../../../Admin/Photos/AddPhotos"
+import { DetailGrid } from "./DetailGrid"
+import { PositionPhoto } from "../../../Admin/Photos/PositionPhoto"
 import { RECIPES,PHOTOS } from "../../../../endpoints"
 import axios from "axios"
+import Button from "../../../Form/Button"
+import FormInput from "../../../Form/FormInput"
 
 export const InstructionHead = (props) => {
-    const { cover_image_url,title,description,pinterest_url,category,published,recipe_id } = props.items[0]
-    // --- FOR TESING ONLY --- //
-    const [ isAdmin,setIsAdmin ] = useState(true)
-    // ----------------------- //
+    const { cover_image_url,title,description,pinterest_url,category,published,recipe_id,servings } = props.items[0]
+    const { isAdmin } = props
+
     const [ formFields,setFormFields ] = useState({
         title:title,
         description:description,
@@ -17,13 +21,13 @@ export const InstructionHead = (props) => {
         recipe_id:recipe_id,
         category:category,
         pinterest_url:pinterest_url,
-        cover_image_url:cover_image_url
+        cover_image_url:cover_image_url,
+        servings:servings
     })
 
     const [ photoPositions,setPhotoPositions ] = useState({
         left:'0px',
         top:'0px',
-        // width:'300px'
     })
 
     useEffect(() => {
@@ -44,6 +48,7 @@ export const InstructionHead = (props) => {
     }
 
     const handleChange = (e) => {
+        e.preventDefault()
         const { name,value } = e.target
         setFormFields({ ...formFields, [name]:value})
     }
@@ -55,10 +60,12 @@ export const InstructionHead = (props) => {
             published,
             recipe_id,
             category,
+            prep_time,
+            servings,
             pinterest_url
         } = formFields
 
-        await putItem(RECIPES.EDIT_RECIPE,{title,description,published,recipe_id,category,pinterest_url,cover_image_url,styling})
+        await putItem(RECIPES.EDIT_RECIPE,{title,description,published,recipe_id,category,pinterest_url,cover_image_url,servings,prep_time,styling})
 
         return
     }
@@ -73,12 +80,8 @@ export const InstructionHead = (props) => {
     return(
         <InstructionHeader>
 
-            <MainImage>
-                <img src={cover_image_url} style={photoPositions} />
-            </MainImage>
-
             {isAdmin ?
-            <Row style={{marginTop:'100px'}} >
+            <Row >
 
                 {cover_image_url === null ? <AddPhoto
                 label={'add photo'}
@@ -92,32 +95,52 @@ export const InstructionHead = (props) => {
                 url={cover_image_url}
                 updateDB={updateCoverImage}
                 />
-                {/* --- adjust photo dimensions function here --- */}
+                {/* --- adjust photo dimensions function here - not currently set up --- */}
+                {isAdmin ? <PositionPhoto /> : null}
                 </>
                 }
 
             </Row> : null}
 
+            <Row style={{}} >
             { !isAdmin ?
-            <h2 style={{marginTop:'100px',color:'#fff',marginBottom:'0px'}}>{title}</h2>
+            <h3 style={{color:'',margin:'auto',fontWeight:'600'}}>{title}</h3>
             :
-            <form>
-                <input
+            <form onSubmit={(e) => putItem(RECIPES.EDIT_RECIPE,formFields)}>
+                <FormInput
                 type="text"
                 name="title"
                 text="text"
+                label="Title"
                 value={formFields.title}
                 onChange={handleChange}
                 />
 
-                <button>update title</button>
+                <Button type="">update title</Button>
             </form>
             }
+            </Row>
 
+            <Row><h4 style={{color:'#555',margin:'auto',fontWeight:''}}>Brittany deMontigny</h4></Row>
 
-            <Row></Row>
-              
-            <Row></Row>
+            <DetailGrid formFields={formFields} setFormFields={setFormFields} handleChange={handleChange} isAdmin={isAdmin} />
+
+            <Row>
+                <DescriptionText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </DescriptionText>
+            </Row>
+
+            <Row style={{width:'95%',backgroundColor:'',display:'flex',justifyContent:'space-between'}} >
+                <Button>print</Button>
+                <Button>pin</Button>
+                <Button>follow</Button>
+            </Row>
+
+            <MainImage>
+                <img src={cover_image_url} style={photoPositions} />
+            </MainImage>
+
             
         </InstructionHeader>
     )
