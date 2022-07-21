1e0483e6ddffe8bde13368ec6e7c1bb1acf98964
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-21 14:41:47 -0600

    Added logo. Set up nav links. Added styled components. Mobile nav is not functional yet

--- bcwb/src/Components/Nav/Nav.jsx
+++ bcwb/src/Components/Nav/Nav.jsx
@@ -1,34 +1,28 @@
-import './Nav.css'
 import Hamburgar from './Hamburger'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import routes from '../../routes'
+import { NavLink,DesktopMenu,MobileMenu,NavBar } from './NavStyles.styles'
+import Logo from '../Assets/Brittanys-Culinary-Creations-v1.png'
 
 const Nav = (props) => {
 
-    const [ menu,setMenu ] = useState(false)
+    const [ isMenuClosed,setMenu ] = useState(true)
+    const menuItems = ['Home','About','Recipes']
+
+    const  mappedItems = menuItems.map((el,i) => {
+        return <NavLink onClick={() => setMenu(!isMenuClosed)} key={i}>{el}</NavLink>
+    })
 
     return(
-        <nav>
-            <ul className='desktop-nav' >
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
-            </ul>
-            <Hamburgar setMenu={setMenu} menu={menu} /> 
-            <Link to='/' ><h1>logo</h1></Link>
-            <h1>search</h1>
-            <ul className={`mobile-nav ${menu ? true : 'closed'}`} >
-                {menu ? <div>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                    <li>item</li>
-                    <p onClick={() => setMenu(false)} >X</p>
-                </div> : null}
-            </ul>
-        </nav>
+        <NavBar>
+            <Link to='/' ><img src={Logo} style={{height:'150px',marginLeft:'40px'}} /></Link>
+
+            <DesktopMenu>{mappedItems}</DesktopMenu>
+
+            <Hamburgar setMenu={setMenu} menu={isMenuClosed} /> 
+
+            <MobileMenu isMenuClosed={isMenuClosed} >{!isMenuClosed ? mappedItems : null}</MobileMenu>
+        </NavBar>
     )
 }
 
