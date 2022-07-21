9e450561da6fa7bf4c2904e183d0746236da0959
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-21 14:44:04 -0600

    Added textarea for editing long text bodies

--- bcwb/src/Components/Form/FormInput.styles.jsx
+++ bcwb/src/Components/Form/FormInput.styles.jsx
@@ -41,6 +41,26 @@ export const Input = styled.input`
   }
 `;
 
+export const TextArea = styled.textarea`
+  background: none;
+  background-color: white;
+  color: ${subColor};
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid ${subColor};
+  margin: 25px 0;
+  &:focus {
+    outline: none;
+  }
+  &:focus ~ ${FormInputLabel} {
+    ${shrinkLabelStyles};
+  }
+`;
+
 export const Group = styled.div`
   position: relative;
   margin: 45px 0;
