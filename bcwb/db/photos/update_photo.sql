e40d9f22d359678d36f29548ead666e4ccdfd4ce
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-21 15:10:27 -0600

    Additional DB elements

--- bcwb/db/photos/update_photo.sql
+++ bcwb/db/photos/update_photo.sql
@@ -1,2 +1,2 @@
 UPDATE bcwb_photos
-SET url = $1, title = $2, album = $3 WHERE photo_id = $4
+SET url = $1, title = $2, album = $3, style_left = $4, style_top = $5, style_width = $6 WHERE photo_id = $7
