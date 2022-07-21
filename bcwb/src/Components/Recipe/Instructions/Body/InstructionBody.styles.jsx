b24e5d62076b83c042fc6257289ef58bc274cbed
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-21 14:55:46 -0600

    Styling adjustment

--- bcwb/src/Components/Recipe/Instructions/Body/InstructionBody.styles.jsx
+++ bcwb/src/Components/Recipe/Instructions/Body/InstructionBody.styles.jsx
@@ -2,9 +2,8 @@ import styled, { css } from 'styled-components';
 
 export const InstructionList = styled.div`
 min-height:350px;
-width: 550px;
+width: 570px;
 padding-right:20px;
-background-color:#fff;
-border: solid 1px #003300;
-border-radius: 0px 0px 20px 20px;
+background-color:#EFFDEC;
+border: solid 1.5px #003300;
 `
