4bb5f6e1b894e65429c2d021f41b48038bb3c91a
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-25 07:54:50 -0600

    Styling adjustments

--- bcwb/src/Components/Home/FindRecipes/FindRecipes.jsx
+++ bcwb/src/Components/Home/FindRecipes/FindRecipes.jsx
@@ -1,6 +1,6 @@
 import '../Home.scss'
 import { useState,useEffect } from 'react'
-import Content from '../Content/Content'
+import { ThumbnailImage,ImageTag } from '../../StyledComponents.styles'
 import data from '../../../data'
 
 const style = {
@@ -28,13 +28,13 @@ const FindRecipes = (props) => {
 
 
 
-    const mappedItems = items.map(el => {
+    const mappedItems = items.slice(0,4).map(el => {
 
-        return <Content key={el.id} img={el.img} text={null} style={style} />
+        return <ThumbnailImage key={el.id} ><img src={el.img} /><ImageTag>Image</ImageTag></ThumbnailImage>
     })    
 
     return(
-        <section style={{hieght:'500px'}} >
+        <section style={{height:'',backgroundColor:'blue'}} >
             {mappedItems}
         </section>
     )
