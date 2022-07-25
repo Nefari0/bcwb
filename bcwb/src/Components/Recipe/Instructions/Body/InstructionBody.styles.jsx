72628f6e551b7a7d3534e5341876041f9da41dd7
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-25 09:01:12 -0600

    Updates

--- bcwb/src/Components/Recipe/Instructions/Body/InstructionBody.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components';
-
-export const InstructionList = styled.div`
-min-height:350px;
-width: 570px;
-padding-right:20px;
-background-color:#EFFDEC;
-border: solid 1.5px #003300;
-`
