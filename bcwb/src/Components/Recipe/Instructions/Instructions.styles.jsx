93f0237edee959ede66826980fb8431a24acac27
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-25 08:04:13 -0600

    re-styling

--- bcwb/src/Components/Recipe/Instructions/Instructions.styles.jsx
+++ bcwb/src/Components/Recipe/Instructions/Instructions.styles.jsx
@@ -4,4 +4,15 @@ export const InstructionMain = styled.main`
 position:absolute;
 margin: auto;
 margin-top: 60px;
+
+min-height:350px;
+width: 550px;
+padding-right:20px;
+padding-left:20px;
+background-color:#EFFDEC;
+border: solid 1.5px #003300;
+display:flex;
+flex-direction:column;
+align-items:baseline;
+text-align:center;
 `
