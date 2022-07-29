4b17896196f8c8f014f92707d9a667aaf0504b02
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-28 18:50:13 -0600

    Downloads category images. Also displays title, Cleaning.

--- bcwb/src/Components/Home/FindRecipes/FindRecipes.jsx
+++ bcwb/src/Components/Home/FindRecipes/FindRecipes.jsx
@@ -1,36 +1,28 @@
 import '../Home.scss'
 import { useState,useEffect } from 'react'
 import { ThumbnailImage,ImageTag } from '../../StyledComponents.styles'
-import data from '../../../data'
-
-const style = {
-    main:{
-        margin:'auto',
-    },
-    img:{
-        height:'200px',
-        width:'200px',
-        borderRadius:'50%'
-    }
-}
+import axios from 'axios'
 
 const FindRecipes = (props) => {
 
     const [ items,setItems ] = useState([])
 
-    useEffect(() => {
-        getDB()
-    },[])
+    useEffect(() => { getDB() },[])
 
     const getDB = () => {
-        setItems([...data])
+        axios.get('/api/category/images/get/all').then(res => {
+            setItems(res.data)
+        })
     }
 
 
 
-    const mappedItems = items.slice(0,4).map(el => {
+    const mappedItems = items.map(el => {
 
-        return <ThumbnailImage key={el.id} ><img src={el.img} /><ImageTag><p style={{marginLeft:'50px',color:'#fff'}}>image</p></ImageTag></ThumbnailImage>
+        return <ThumbnailImage key={el.link_id} >
+                    <img src={el.photo_link} />
+                    <ImageTag>{el.category}</ImageTag>
+                </ThumbnailImage>
     })    
 
     return(
