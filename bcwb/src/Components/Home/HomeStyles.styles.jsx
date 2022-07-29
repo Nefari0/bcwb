d8fc5122d2e6677a08f928ace61921e17d65f8a8
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-28 18:41:41 -0600

    Will be moving all Home.scss items to this code

---
+++ bcwb/src/Components/Home/HomeStyles.styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { colors } from '../Styles/colors';
+
+const { darkText } = colors
+
+export const SectionText = styled.h4`
+    text-align:left;
+    color:${darkText}
+    border-bottom: solid 1px ${darkText}
+    margin-left:10px;
+    font-family: 'Cedarville Cursive', cursive;
+ `
