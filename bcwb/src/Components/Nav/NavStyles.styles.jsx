5a2f282f87c6772ab7fa6b858c5f8a7e0e204730
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-28 19:30:53 -0600

    Fully responsive

--- bcwb/src/Components/Nav/NavStyles.styles.jsx
+++ bcwb/src/Components/Nav/NavStyles.styles.jsx
@@ -1,13 +1,13 @@
 import styled, { css } from 'styled-components';
+import { colors } from '../Styles/colors';
 
-const baseColor = '#003300;'
+const { baseColor } = colors
 
 
 const menuClosed = css`
 height: 0px;
 width: 0px;
 transition: 300ms ease all;
-// display:none;
 `
 
 export const NavBar = styled.nav`
@@ -16,13 +16,17 @@ export const NavBar = styled.nav`
   justify-content: space-around;
   align-items: center;
   height: 300px;
-  min-width:300px;
-  border-bottom: solid 1px ${baseColor}
-  overflow:hidden
+  min-width:400px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.35);
+  margin-bottom:20px;
+
+  @media (max-width:800px){ height:200px; }
+
+  @media (max-width:400px) { height:100px; }
 `
 
 export const NavLink = styled.a`
-  font-size:40px;
+  font-size:36px;
   font-style:italic;
   z-index: 4;
   color:${baseColor};
@@ -31,9 +35,7 @@ export const DesktopMenu = styled.div`
   width:100%;
   display:flex;
   justify-content:space-around;
-  @media (max-width:1250px) {
-    display:none;
-  }
+  @media (max-width:1250px) { display:none; }
 `
 
 export const MobileMenu = styled.div`
@@ -50,5 +52,8 @@ export const MobileMenu = styled.div`
 `
 
 export const LogoBox = styled.img`
-  height:450px;
+  max-width:1000px;
+  @media (max-width:800px){
+    max-width:600px;
+  }
 `
