074b8a6534e6ef511946c091ba1690a22d918565
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-28 19:52:47 -0600

    Footer component

--- bcwb/src/App.jsx
+++ bcwb/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.scss';
 import Nav from './Components/Nav/Nav';
+import { Footer } from './Components/Footer/Footer';
 import routes from './routes';
 import { HashRouter } from 'react-router-dom'
 
 function App() {
   return (
     <HashRouter>
-    <div className='App' >
+    <div >
         <Nav />
         {routes}
+        <Footer />
     </div>
     </HashRouter>
   );
