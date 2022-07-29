349d16afa1053ac2f965aeb7db9e5b0b615b5df2
Nefari0 <chrisdemontigny@yahoo.com>
2022-07-28 19:15:07 -0600

    Colors used in app will be imported from here

---
+++ bcwb/src/Components/Styles/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+    baseColor:'#003300;',
+    secondaryColor:'#4BA347;',
+    lightPaper:'#EFFDEC;',
+    yellowPaper:'#FFFBCE;',
+    white:'#fff;',
+    darkText:'#555;',
+}
