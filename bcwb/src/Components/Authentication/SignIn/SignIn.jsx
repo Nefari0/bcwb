d4b17ec5b582e3edae8b67689fea4f77cb7ea0b9
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-01 15:54:15 -0600

    SignIn and SignUp features. Create accound using google login or email

---
+++ bcwb/src/Components/Authentication/SignIn/SignIn.jsx
@@ -0,0 +1,99 @@
+import FormInput from "../../Form/FormInput"
+import { Form } from "../../Form/FormInput.styles"
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
+import { useState } from "react"
+import { BaseButton,GoogleSignInButton,InvertedButton } from "../../Form/Button.styles"
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInAuthUserWithEmailAndPassword
+} from "../../../base"
+
+const defaultState = {
+    email:'',
+    password:'',
+}
+
+const SignIn = (props) => {
+
+    const [ formFields,setFormFields ] = useState(defaultState)
+    const { password,email } = formFields
+
+    const resetForm = () => {
+        setFormFields(defaultState)
+    }
+
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user)
+        await props.history.push(`/home/${userDocRef.id}`)
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault()
+        const  { name,value } = event.target
+        setFormFields({...formFields, [name]:value})
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        
+        try {
+
+            const { user } = await signInAuthUserWithEmailAndPassword(email,password)
+
+            props.history.push(`/home/${user.uid}`)
+            resetForm()
+        } catch (error) {
+            switch (error.code) {
+                case "auth/wrong-password": 
+                    alert('Incorrect password')
+                    break;
+                case "auth/user-not-found":
+                    alert(`User with email address ${email} does not exist`)
+                    break;
+                case "auth/too-many-requests":
+                    alert('Too many login attempts. Please reset your password or try again later.')
+                default:
+                    console.log(error)
+            }
+        }
+
+
+    }
+
+    return(
+        <Form onSubmit={handleSubmit}>
+
+            <h4>Have an account?</h4>
+
+            <FormInput
+                label='Email'
+                name='email'
+                type='text'
+                 required
+                onChange={handleChange} 
+                value={email}
+            />
+
+            <FormInput
+                label='Password'
+                name='password'
+                type='text'
+                 required
+                onChange={handleChange} 
+                value={password}
+            />
+
+            <BaseButton type="submit" >Sign In</BaseButton>
+
+            <GoogleSignInButton onClick={() => logGoogleUser()}>sign in with google</GoogleSignInButton>
+
+            <Link to="/signup" style={{textDecoration:'none'}} ><InvertedButton>Dont have an account?</InvertedButton></Link>
+
+        </Form>
+    )
+}
+
+export default withRouter(SignIn)
---
+++ bcwb/src/Components/Authentication/SignUp/SignUp.jsx
@@ -0,0 +1,126 @@
+import FormInput from "../../Form/FormInput";
+import { useState } from "react";
+import { BaseButton, GoogleSignInButton, InvertedButton } from "../../Form/Button.styles";
+import { Form } from "../../Form/FormInput.styles";
+// import { SectionText } from "../../StyledComponents.styles";
+import { signInWithGooglePopup,createUserDocumentFromAuth,createAuthUserWithEmailAndPassword } from "../../../base"
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom' 
+// import { createAuthUserWithEmailAndPassword } from "../../base";
+
+const defaultState = {
+    displayName:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+}
+
+const SignUp = (props) => {
+
+    const [ formFields,setFormFields ] = useState(defaultState)
+    const { email,password,displayName,confirmPassword } = formFields
+
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user)
+        props.history.push(`/home/${userDocRef.id}`)
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault()
+        const  { name,value } = event.target
+        setFormFields({...formFields, [name]:value})
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        
+        if(password != confirmPassword) {
+            alert("Passwords do not match")
+            return
+        }
+        
+        
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(email,password);
+
+            props.history.push(`/home/${user.uid}`)
+
+            await createUserDocumentFromAuth(user,{ displayName })
+            
+            resetForm()
+
+        } catch(error) {
+            switch (error.code) {
+                case "auth/email-already-in-use":
+                    alert('This email is already being used. Please choose another email')
+                    break;
+                case "auth/invalid-email":
+                    alert("Invalid email address")
+                default:
+                    console.log('user creation encountered an error',error);
+            }
+        }
+    }
+
+    const resetForm = () => {
+        setFormFields(defaultState)
+    }
+
+    return(
+        
+        <Form onSubmit={handleSubmit} >
+
+            <h4>Dont have an account</h4>
+            
+            <FormInput
+             label='Email'
+             name='email'
+             type='text'
+             required
+             onChange={handleChange} 
+             value={email}
+            />
+
+            <FormInput
+             label='Display Name'
+             name='displayName'
+             type='text'
+             required
+             onChange={handleChange} 
+             value={displayName}
+            />
+
+
+            <FormInput
+             label='Password'
+             name='password'
+             type='text'
+             required
+             onChange={handleChange} 
+             value={password}
+            />
+
+            <FormInput
+             label='Confirm Password'
+             name='confirmPassword'
+             type='text'
+             required
+             onChange={handleChange} 
+             value={confirmPassword}
+            />
+
+            <BaseButton type="submit" >Sign Up</BaseButton>
+
+            <GoogleSignInButton onClick={() => logGoogleUser()}>sign in with google</GoogleSignInButton>
+
+            <Link to="/signin" style={{textDecoration:'none'}} ><InvertedButton>already have an account?</InvertedButton></Link>
+
+        </Form>
+        
+    )
+}
+
+export default withRouter(SignUp)
+
+// export default SignIn
