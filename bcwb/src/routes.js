a8c73ba3251787de02d5bf021d65ef455b5b5464
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-01 14:58:50 -0600

    Route to SignIn and SignUp components

--- bcwb/src/routes.js
+++ bcwb/src/routes.js
@@ -2,11 +2,16 @@ import { Switch,Routes,Route,BrowserRouter as Router } from 'react-router-dom'
 import Admin from './Components/Admin/Admin'
 import Home from './Components/Home/Home'
 import Recipe from './Components/Recipe/Recipe'
+import SignUp from './Components/Authentication/SignUp/SignUp'
+import SignIn from './Components/Authentication/SignIn/SignIn'
 
 export default (
     <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/home/:id" component={Home} />
         <Route path="/admin" component={Admin} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
         <Route path="/recipe/:recipe_id" component={Recipe} />
     </Switch>
 )
