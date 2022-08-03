f029d6c28f5bd7fe9bd55fb07ce4a82985bdc3f1
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-02 23:18:14 -0600

    User info in context. Access to Admin page

--- bcwb/src/Components/Nav/Nav.jsx
+++ bcwb/src/Components/Nav/Nav.jsx
@@ -1,13 +1,27 @@
 import Hamburgar from './Hamburger'
-import { useState } from 'react'
+import { useState,useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { NavLink,DesktopMenu,MobileMenu,NavBar,LogoBox } from './NavStyles.styles'
 import Logo from '../Assets/Brittanys-Culinary-Creations-v1.png'
+import { UserContext } from '../Context/user.context'
+import { signOutUser } from '../../base'
+import { BaseButton,DecoButtonWrapper } from '../Form/Button.styles'
+import { styles } from '../Styles/customStyles'
+import access from '../../access'
 
-const Nav = (props) => {
+const { decoButton } = styles
+
+const Nav = () => {
 
     const [ isMenuClosed,setMenu ] = useState(true)
     const menuItems = ['Home','About','Recipes']
+
+    const signOutHandler = async () => {
+        await signOutUser()
+        setCurrentUser(null)
+    }
+
+    const { currentUser,setCurrentUser } = useContext(UserContext)
 
     const  mappedItems = menuItems.map((el,i) => {
         return <NavLink onClick={() => setMenu(!isMenuClosed)} key={i}>{el}</NavLink>
@@ -15,7 +29,29 @@ const Nav = (props) => {
 
     return(
         <NavBar>
-            <Link to='/' ><LogoBox src={Logo} /></Link>
+
+            {currentUser != null && access.getAccess(currentUser.uid) === "ACCESS_GRANTED" ? 
+
+            <i><Link to="/admin">Admin</Link></i>
+            : 
+            null
+
+            }
+
+            <Link to={`/`} ><LogoBox src={Logo} /></Link>
+
+            <DecoButtonWrapper>
+                {currentUser ? 
+
+                <BaseButton style={decoButton} onClick={signOutHandler} >sign out</BaseButton>
+                :
+                <Link to="/signin">
+                    <BaseButton style={decoButton}>sign in</BaseButton>
+                </Link>
+
+                }
+            </DecoButtonWrapper>
+
             {/* *** THESE FEATURES ARE TEMPORARILY DISABLED UNTIL DESIGN IS UPDATED *** */}
 
             {/* <DesktopMenu>{mappedItems}</DesktopMenu> */}
