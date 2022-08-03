8551f2c2609f6d0dfdb609aaed6e9098f5b93680
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-02 23:01:50 -0600

    Using Context for user info managment

--- bcwb/src/index.js
+++ bcwb/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from './Components/Context/user.context';
 import { Provider } from 'react-redux'
 import store from './ducks/store';
 import { HashRouter, BrowserRouter } from 'react-router-dom';
@@ -12,9 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <Provider store={store} >
-    <App />
-    </Provider>
+      <UserProvider>
+        <Provider store={store} >
+            <App />
+        </Provider>
+      </UserProvider>
     </Router>
   </React.StrictMode>
 );
