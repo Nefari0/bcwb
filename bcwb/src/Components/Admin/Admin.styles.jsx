2df6df20304b140d191ce1b29a13220dd1b708d1
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-05 04:52:28 -0600

    Styling for Admin.component.jsx

---
+++ bcwb/src/Components/Admin/Admin.styles.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const AdminBox = styled.main`
+    min-width: 300px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const AdminHeader = styled.header`
+    display:flex;
+
+    h4 {
+        font-size:16px;
+        font-weight:200;
+    }
+`
