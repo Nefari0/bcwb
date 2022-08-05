815ebd8248a70b9f0c4fd8a412fad85abcd1e7c5
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-05 04:51:18 -0600

    Replaces Admin.jsx. New views, including categories management section

---
+++ bcwb/src/Components/Admin/Admin.component.jsx
@@ -0,0 +1,49 @@
+import './Admin.scss'
+import CreateRecipe from './CreateRecipe/CreateRecipe'
+import { ViewRecipes } from './ViewRecipes/ViewRecipes'
+import Spinner from '../Spinner/spinner.component';
+import {updateCharacters} from '../../ducks/breakingBadReducer';
+import { getRecipes } from '../../ducks/recipeReducer';
+import { connect } from 'react-redux'
+import { useEffect,useState } from 'react';
+import { AdminHeader,AdminBox } from './Admin.styles';
+import ViewCategories from './Categories/ViewCategories.component';
+
+const Admin = (props) => {
+
+    useEffect(() => {
+        getRecipes()
+    },[])
+    
+    const [ currentView,setCurrentView ] = useState('VIEW_RECIPES')
+
+    const changeView = (view) => {
+        setCurrentView(view)
+        window.scrollTo({
+            top: 1200,
+            behavior: 'smooth'
+        });
+    }
+
+    
+    
+    return(
+        <AdminBox>
+            <AdminHeader>
+                <h4 onClick={() => changeView('VIEW_RECIPES')} >recipes</h4>
+                <h4 >new recipe</h4>
+                <h4 onClick={() => changeView('VIEW_CATEGORIES')} >categories</h4>
+            </AdminHeader>
+            <CreateRecipe />
+            {props.recipes.isLoading ? <Spinner /> : null}
+            {currentView === 'VIEW_CATEGORIES' ? <ViewCategories /> : null}
+            {currentView === 'VIEW_RECIPES' ? <ViewRecipes recipes={props.recipes} /> : null}
+        </AdminBox>
+    )
+}
+
+function mapStateToProps(reduxState) {
+    return reduxState
+}
+
+export default connect(mapStateToProps, { updateCharacters,getRecipes })(Admin)
