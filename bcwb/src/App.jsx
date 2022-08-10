2e564f3b803929f375818da184766e6e46e77fb7
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-10 10:56:08 -0600

    Spinner is rendered from this component globally. Added redux since it is activated in recipeReducer.js.

--- bcwb/src/App.jsx
+++ bcwb/src/App.jsx
@@ -3,17 +3,28 @@ import Nav from './Components/Nav/Nav';
 import routes from './routes';
 import { HashRouter } from 'react-router-dom'
 import { MobileUserBar } from './Components/MobileUserBar/MobileUserBar.component';
+import Spinner from './Components/Spinner/spinner.component';
+import { setSpinner } from './ducks/recipeReducer';
+import { connect } from 'react-redux'
 
-function App() {
+function App(props) {
+
+  const { isLoading } = props.recipes
+
   return (
     <HashRouter>
     <div >
         <Nav />
         <MobileUserBar />
+        {isLoading ? <Spinner /> : null}
         {routes}
     </div>
     </HashRouter>
   );
 }
 
-export default App;
+function mapStateToProps(reduxState) {
+  return reduxState
+}
+
+export default connect(mapStateToProps, {setSpinner})(App)
