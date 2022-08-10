9daf84edfe934ef28363ef05e9d1d183aafcb079
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-10 10:37:26 -0600

    Changed display and styling settings, but these are probably just temporary adjustments

--- bcwb/src/Components/Admin/ViewRecipes/RecipeListItem.jsx
+++ bcwb/src/Components/Admin/ViewRecipes/RecipeListItem.jsx
@@ -1,14 +1,12 @@
-import { ThumbnailImage } from "../../StyledComponents.styles";
+import { ThumbnailImage,ImageTag } from "../../StyledComponents.styles";
 
 export const RecipeListItem = (props) => {
     
     const { items } = props
-    const { title } = props.items
 
     return (
-        <>
-            <ThumbnailImage><img src={items.cover_image_url} /></ThumbnailImage>
-            <h4>{items.title}</h4>
-        </>
+        <ThumbnailImage><img src={items.cover_image_url} />
+            <ImageTag>{items.title}</ImageTag>
+        </ThumbnailImage>
     )
 }
