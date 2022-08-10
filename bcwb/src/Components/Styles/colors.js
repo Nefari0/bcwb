ac15ecdd3945b25bb911d967fea418038316683e
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-09 21:54:10 -0600

    Updated all colors for use in jsx as well as css. Added color gradients as well

--- bcwb/src/Components/Styles/colors.js
+++ bcwb/src/Components/Styles/colors.js
@@ -1,9 +1,23 @@
+import { css } from 'styled-components'
+
 export const colors = {
-    baseColor:'#003300;',
-    secondaryColor:'#4BA347;',
-    lightPaper:'#EFFDEC;',
-    yellowPaper:'#FFFBCE;',
-    white:'#fff;',
-    darkText:'#555;',
-    tags:'#E67E22;'
+    baseColor:'#003300',
+    secondaryColor:'#4BA347',
+    lightPaper:'#EFFDEC',
+    yellowPaper:'#FFFBCE',
+    white:'#fff',
+    darkText:'#555',
+    tags:'#E67E22'
+}
+
+export const baseGradient = css`
+    background: linear-gradient(0deg,${colors.baseColor},${colors.secondaryColor});
+`
+export const secondaryGradient = css`
+    background: linear-gradient(0deg,${colors.secondaryColor},${colors.lightPaper});
+`
+
+// *** JSX GRADIENTS *** //
+export const secondaryGradientJSX = {
+    background: `linearGradient(0deg,${colors.secondaryColor},${colors.lightPaper})`
 }
