fb0207672f20b55b02418c06e4c1c3a5a8c2ae07
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-10 11:00:30 -0600

    RecipeProvider

--- bcwb/src/index.js
+++ bcwb/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './Components/Context/user.context';
+import { RecipeProvider } from './Components/Context/recipe.context';
 import { Provider } from 'react-redux'
 import store from './ducks/store';
 import { HashRouter, BrowserRouter } from 'react-router-dom';
@@ -13,11 +14,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <RecipeProvider>
       <UserProvider>
         <Provider store={store} >
             <App />
         </Provider>
       </UserProvider>
+      </RecipeProvider>
     </Router>
   </React.StrictMode>
 );
