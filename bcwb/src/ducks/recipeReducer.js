0d7ebab9a1e87c295bdb7901c52842cad516a95e
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-11 21:03:09 -0600

    Pulled endpoints in from endpoints.js. Added categories to reducer to share between "CreateCategory" and "CreateRecipe"

--- bcwb/src/ducks/recipeReducer.js
+++ bcwb/src/ducks/recipeReducer.js
@@ -1,19 +1,33 @@
 import axios from "axios";
+import { CATEGORIES,RECIPES } from "../endpoints";
+
+const { GET_ALL_CATEGORIES } = CATEGORIES
+const { GET_RECIPES } = RECIPES
 
 const initialState = {
   recipes: {},
+  categories: {},
+  errorMessage:'',
   isLoading:false,
 };
 
-const GET_RECIPES = "GET_RECIPES";
+const FETCH_RECIPES = "FETCH_RECIPES";
+const FETCH_CATEGORIES = "FETCH_CATEGORIES";
 const SET_SPINNER = "SET_SPINNER";
 
 export function getRecipes() {
   return {
-    type: GET_RECIPES,
-    payload: axios.get('/api/recipes/get/all')
+    type: FETCH_RECIPES,
+    payload: axios.get(GET_RECIPES)
 
   };
+};
+
+export function getCategories() {
+  return {
+    type: FETCH_CATEGORIES,
+    payload: axios.get(GET_ALL_CATEGORIES)
+  }
 }
 
 export function setSpinner(val) {
@@ -25,22 +39,44 @@ export function setSpinner(val) {
 
 export default function recipeReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_RECIPES + "_PENDING":
+
+    //  --- Get all categories --- //
+    case FETCH_CATEGORIES + '_PENDING':
+      return {
+        ...state,
+        isLoading:true,
+      };
+      case FETCH_CATEGORIES + '_FULFILLED':
+        return {
+          ...state,
+          categories: action.payload.data,
+          isLoading:false
+        }
+      case FETCH_CATEGORIES + '_REJECTED':
+        return {
+          ...state,
+          isLoading:false,
+          errorMessage:'not found'
+        };
+
+    // --- Get Recipes --- //
+    case FETCH_RECIPES + "_PENDING":
       return {
         ...state,
         isLoading:true
       };
-    case GET_RECIPES + "_FULFILLED":
+    case FETCH_RECIPES + "_FULFILLED":
         return {
             ...state,
             recipes: action.payload,
             isLoading:false
         };
-    case GET_RECIPES + "_REJECTED":
+    case FETCH_RECIPES + "_REJECTED":
         return {
             ...state,
             isLoading:false
         }
+
     // -- Spinner / load screen controller -- //
     case SET_SPINNER:
         return {
