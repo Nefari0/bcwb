76816da82726f2905feccedbfe42f006799891dd
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-16 11:25:41 -0600

    Get published recipes and photos. Only recipes with non-null cover_photo_urls get pulled

--- bcwb/db/recipe/get_published_recipes.sql
+++ bcwb/db/recipe/get_published_recipes.sql
@@ -1,1 +1,5 @@
-SELECT * FROM recipe WHERE published = true
+SELECT * FROM recipe r
+JOIN bcwb_photos bp ON r.cover_image_url = bp.url AND r.published = true
+
+-- ORIGINAL BELOW --
+-- SELECT * FROM recipe WHERE published = true
