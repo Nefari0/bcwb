529abee7aa104728d64ececa98ed822280b2a74f
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-16 11:27:11 -0600

    hours and minutes columns

--- bcwb/db/recipe/update_recipe.sql
+++ bcwb/db/recipe/update_recipe.sql
@@ -1,3 +1,3 @@
 UPDATE recipe
-SET title = $1, description = $2, pinterest_url = $3, category = $4, published = $5, cover_image_url = $6, servings = $7, prep_time = $8, author = $9 WHERE recipe_id = $10
+SET title = $1, description = $2, pinterest_url = $3, category = $4, published = $5, cover_image_url = $6, servings = $7, hours = $8, author = $9, minutes = $10 WHERE recipe_id = $11
 RETURNING *;
