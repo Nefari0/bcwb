68dc9f5126072aabe8e9f475b2fbb36a2394296d
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-16 11:55:26 -0600

    Adjustments

--- bcwb/src/Components/StyledComponents.styles.jsx
+++ bcwb/src/Components/StyledComponents.styles.jsx
@@ -3,34 +3,11 @@ import styled from 'styled-components';
 import { colors } from './Styles/colors';
 import { device } from './Styles/queries';
 
-const { mobileL,tablet } = device
+const { mobileL } = device
 const { lightPaper,baseColor } = colors
 
-// export const Error = styled.div`
-//   position:fixed;
-//   top:40%;
-//   width:500px;
-//   background-color:grey;
-//   color:#fff;
-//   z-index:100000;
-//   border-radius:5px;
-//   padding:10px;
-//   display:flex;
-//   flex-direction:column;
-//   box-shadow: 0px 5px 20px -7px #000000;
-
-//   @media ${mobileL} {
-//     top:20%;
-//     width:295px;
-//   }
-// `;
-
 export const PortraitImage = styled.div`
-
- 
-  // margin:10px;
   margin:auto;
-  background-color:red;
   height:400px;
   width:267px;
   border-radius:10px;
