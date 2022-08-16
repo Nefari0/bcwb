9e456dad049fa48fc6a9655c2a17b55b9e78aa05
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-15 22:28:22 -0600

    Styling adjustments

--- bcwb/src/Components/Home/RecipeCover/recipe.styles.jsx
+++ bcwb/src/Components/Home/RecipeCover/recipe.styles.jsx
@@ -1,25 +1,24 @@
 import styled from 'styled-components'
 import { colors } from '../../Styles/colors'
 
-const { baseColor } = colors
+const { baseColor,darkText,lightPaper } = colors
 
 export const CoverContainer = styled.div`
-position:relative;
+    position:relative;
     min-height:400px;
-    width:300px;
-    // background-color:blue;
+    min-width:280px;
+    max-width:300px;
     text-align:center;
     display:flex;
     flex-direction:column;
+    align-items:center;
+    background-color: ${lightPaper};
+    border: solid 1px ${baseColor};
 
     img,
     h5,
     h3 {
         margin:5px;
-    }
-
-    img {
-        
     }
 
     h3 {
@@ -36,15 +35,28 @@ position:relative;
         height:0px;
         position:relative;
         margin:auto;
-        padding-bottom:6px;
+        margin:20px 0px 10px 0px;
+        padding-bottom:8px;
 
         h5 {
             margin:auto;
-            // position:absolute;
-            // top:-0px;
-            background-color:#fff;
+            background-color: ${lightPaper};
+            padding:0px 5px 0px 5px;
             width:60px;
-            font-size: 10px;
+            font-size: 12px;
+            text-transform:uppercase;
         }
+    }
+
+    strong {
+        color: ${darkText};
+        min-width:10px;
+        margin:10px 0px 0px 0px;
+        padding-left:0px;
+        padding-bottom:10px;
+    }
+
+    button {
+        width:50%;
     }
 `
