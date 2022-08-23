f4817b5c81f5819af603e47dccf377986e3c1c1d
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-22 23:27:15 -0600

    Added dummy object to ThumbnailImage.jsx. I plan to eliminate this requirement

--- bcwb/src/Components/Admin/ViewRecipes/RecipeListItem.jsx
+++ bcwb/src/Components/Admin/ViewRecipes/RecipeListItem.jsx
@@ -1,12 +1,25 @@
-import { ThumbnailImage,ImageTag } from "../../StyledComponents.styles";
+import { ThumbnailImage,ImageTag } from "../../Styles/Images/images.styles";
 
 export const RecipeListItem = (props) => {
     
     const { items } = props
 
+    const propObject = {
+        category_id:0,
+        selectedCategory:null,
+        setSelectedCategory:null
+    }
+
+    const style = {
+        left:'-0px',
+        top:'10px',
+        transform:'rotate(0deg)'
+    }
+
     return (
-        <ThumbnailImage><img src={items.cover_image_url} />
-            <ImageTag>{items.title}</ImageTag>
+        <ThumbnailImage propObject={propObject}>
+            <img src={items.cover_image_url} />
+            <ImageTag style={style}>{items.title}</ImageTag>
         </ThumbnailImage>
     )
 }
