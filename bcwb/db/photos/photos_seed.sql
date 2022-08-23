e9c9885ef969458871b88f9107cd12d94e6842d4
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-22 23:07:37 -0600

    Rename "title" column to "photo_name" to resolve conflicted

--- bcwb/db/photos/photos_seed.sql
+++ bcwb/db/photos/photos_seed.sql
@@ -2,7 +2,7 @@ CREATE TABLE bcwb_photos (
   photo_id SERIAL PRIMARY KEY,
   url TEXT,
   title varchar(250),
-  album varchar(250),
+  photo_name varchar(250),
   style_left INTEGER,
   style_top INTEGER,
 )
