7596bd1452875046314087f10cdc6e16547c82eb
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-22 23:21:01 -0600

    Had to pass dummy object to ThumbnailImage.jsx. I plan to fix this issue

--- bcwb/src/Components/Admin/Categories/CatItem.component.jsx
+++ bcwb/src/Components/Admin/Categories/CatItem.component.jsx
@@ -1,13 +1,30 @@
-import { ThumbnailImage,ImageTag } from "../../StyledComponents.styles"
+import { ThumbnailImage,ImageTag } from "../../Styles/Images/images.styles"
 
 const CatItem = (props) => {
 
     const { category,photo_url,selectFunction } = props
 
+
+    // --- This is the default object for items in ThumbnailImage - Will be removing this requirement --- //
+    const propObject = {
+        category_id:null,
+        selectedCategory:null,
+        setSelectedCategory:selectFunction
+    }
+
     return(
-        <ThumbnailImage onClick={(e) => selectFunction(e,props)} >
-            <img src={photo_url} />
+        <ThumbnailImage
+            propObject={propObject}
+            onClick={(e) => 
+                {e.preventDefault()
+                selectFunction(e,props)}
+            }
+        >
+
+            <img src={photo_url}  />
+
             <ImageTag>{category}</ImageTag>
+            
         </ThumbnailImage>
     )
 }
