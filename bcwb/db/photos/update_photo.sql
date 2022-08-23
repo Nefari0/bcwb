aaf0a922f94830f1743b7181ab054f49e58b2ec5
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-22 23:10:17 -0600

    Renamed "title" to "photo_name" to resolve conflict

--- bcwb/db/photos/update_photo.sql
+++ bcwb/db/photos/update_photo.sql
@@ -1,2 +1,2 @@
 UPDATE bcwb_photos
-SET url = $1, title = $2, album = $3, x = $4, y = $5, z = $6 WHERE photo_id = $7
+SET url = $1, photo_name = $2, album = $3, x = $4, y = $5, z = $6 WHERE photo_id = $7
