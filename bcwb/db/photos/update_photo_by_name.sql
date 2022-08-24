c3b232b59d7440a2c5c9b76d8099bc73c110b203
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-24 13:14:42 -0600

    Deleted

--- bcwb/db/photos/update_photo_by_name.sql
+++
@@ -1,4 +0,0 @@
--- This udpates an existing row during the  "add new photo" query
-UPDATE bcwb_photos
-SET url = $1 WHERE photo_name = $2
-RETURNING *;
--- bcwb/src/Components/Home/LatestRecipes/LatestRecipeItem.jsx
+++
@@ -1,9 +0,0 @@
-// import { PortraitImage,ImageTag,MainImage } from "../../StyledComponents.styles"
-// import { colors } from "../../Styles/colors"
-
-// const { baseColor } = colors
-
-// export const LatestRecipeItem = ({img,title,id}) => {
-
-//     return(<MainImage style={{position:'relative'}}><img src={img} /><ImageTag style={{color:`${baseColor}`}} >{title}</ImageTag></MainImage>)
-// }
