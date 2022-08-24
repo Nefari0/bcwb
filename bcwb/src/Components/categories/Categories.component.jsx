c89847f187ebf1f159a3b0e893b129cb690a7126
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-24 12:49:17 -0600

    Loads recipes from recipeReducer. Displays recipes by current category

--- bcwb/src/Components/categories/Categories.component.jsx
+++ bcwb/src/Components/categories/Categories.component.jsx
@@ -1,37 +1,46 @@
-import axios from "axios"
+// import axios from "axios"
+import { connect } from 'react-redux'
+import { getRecipes } from "../../ducks/recipeReducer"
 import { SmallRecipeItem } from "../SmallRecipeItem/SmallRecipeItem.component"
 import { useEffect, useState } from "react"
 import { CategoriesContainer } from "./categories.styles"
-import { RECIPES } from "../../endpoints"
-const { GET_BY_CATEGORY } = RECIPES
+// import { RECIPES } from "../../endpoints"
+// const { GET_BY_CATEGORY } = RECIPES
  
 const Categories = (props) => {
     const { category } = props.match.params
     const [ items,setItems ] = useState([])
 
-    useEffect(() => {getItems()},[])
+    useEffect(() => {
+        const gettingCatetories = async () => {
+            await props.getRecipes().then(res => setItems(res.value.data))
+        }
+        if (items[0] === undefined) {gettingCatetories()}
+    },[])
 
-    const getItems = () => {
-        axios.post(GET_BY_CATEGORY,{category}).then(res => {
-            setItems(res.data)
-        })
-    }
+    const displayedCats = items.filter(el => {
+        return el.category === category
+    })
 
-    const mappedItems = items.map(el => {
+    const mappedItems = displayedCats.map(el => {
         var styles = {
             width:`${el.z + 50}px`,
             height:'auto',
             position:'absolute'
         }
-        return <SmallRecipeItem key={el.recipe_id} img={el.cover_image_url} title={el.title} id={el.recipe_id} styles={styles} />
+        return <SmallRecipeItem key={el.recipe_id} img={el.cover_image_url} title={el.title} id={el.recipe_id} style={styles} />
     })
 
     return (
     <CategoriesContainer>
-        <header>{category}</header>
+        <header>{category}<span></span></header>
         <section>{mappedItems}</section>
     </CategoriesContainer>
     )
 }
 
-export default Categories
+function mapStateToProps(reduxState) {
+    return reduxState
+}
+
+export default connect(mapStateToProps, {getRecipes})(Categories)
