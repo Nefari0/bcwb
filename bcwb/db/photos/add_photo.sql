3c427fb8a25cab600aa150e0e2d3cdb66121714e
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-29 21:59:51 -0600

    Adds angle paremeter to bcwb_photos db

--- bcwb/db/photos/add_photo.sql
+++ bcwb/db/photos/add_photo.sql
@@ -1,3 +1,3 @@
-INSERT INTO bcwb_photos (url,photo_name,album,x,y,z)
-VALUES ($1,$2,$3,$4,$5,$6)
+INSERT INTO bcwb_photos (url,photo_name,album,x,y,z,angle)
+VALUES ($1,$2,$3,$4,$5,$6,$7)
 RETURNING *;
