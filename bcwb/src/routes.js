f87e8977a6502545ce6cb50f790a8ea197b74ee1
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-31 17:05:42 -0600

    LatestRecipes route. Removed both Home routes

--- bcwb/src/routes.js
+++ bcwb/src/routes.js
@@ -1,15 +1,14 @@
 import { Switch,Routes,Route,BrowserRouter as Router } from 'react-router-dom'
 import Admin from './Components/Admin/Admin.component'
-import Home from './Components/Home/Home'
 import Recipe from './Components/Recipe/Recipe'
 import SignUp from './Components/Authentication/SignUp/SignUp'
 import SignIn from './Components/Authentication/SignIn/SignIn'
 import Categories from './Components/categories/Categories.component'
+import LatestRecipes from './Components/LatestRecipes/latest.component'
 
 export default (
     <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/home/:id" component={Home} />
+        <Route exact path="/" component={LatestRecipes} />
         <Route path="/admin" component={Admin} />
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
