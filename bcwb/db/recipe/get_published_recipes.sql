bd6ab8583c23db268f9c51fb3eb55236a4bef13b
Nefari0 <chrisdemontigny@yahoo.com>
2022-08-31 20:04:36 -0600

    Order by most recent

--- bcwb/db/recipe/get_published_recipes.sql
+++ bcwb/db/recipe/get_published_recipes.sql
@@ -1,5 +1,3 @@
 SELECT * FROM recipe r
 JOIN bcwb_photos bp ON r.cover_image_url = bp.url AND r.published = true
-
--- ORIGINAL BELOW --
--- SELECT * FROM recipe WHERE published = true
+ORDER BY date_created DESC
