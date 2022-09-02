2c9d9655029b00313ebaaedf2fe17599c33410e0
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-01 21:51:28 -0600

    Wrapped the Thumbnail in a span to create carousel effect. The span moves under certain conditions

--- bcwb/src/Components/Nav/content.component.jsx
+++ bcwb/src/Components/Nav/content.component.jsx
@@ -1,12 +1,24 @@
 import { changeView } from "../../ducks/navReducer"
 import { connect } from 'react-redux'
 import { ThumbnailImage,ImageTag } from "../Styles/Images/images.styles"
+
 import { Link } from 'react-router-dom'
+import { useState,useEffect } from "react"
 
 const Content = (props) => {
 
-    const { content } = props
+    const { content,index } = props
     const { currentCategory } = props.currentCategory
+    const [style,setStyle] = useState(null)
+
+    useEffect(() => {overrideStyle()}, [])
+    
+    // --- Moving last element of array to first --- //
+    const overrideStyle = () => {
+        if (index === 0) {
+            setStyle(0)
+        } else {setStyle(null)}
+    }
 
     const {
         x,y,z,
@@ -20,14 +32,23 @@ const Content = (props) => {
         top:`${y}px`,
         width:`${z}px`,
         position:'absolute',
+        margin:'auto'
+    }
+
+    // --- Move element to back of array --- //
+    const wrapper = {
+        left:`${style}px`,
+        transition:'all 500ms'
     }
 
     return (
+            <span index={index}>
             <ThumbnailImage props={{category_id,currentCategory}} onClick={() => props.changeView(category_id)}>
                 <Link to={`/categories/${category}`}  ><img src={photo_url} style={positions} />
                     <ImageTag label={category}>{category}</ImageTag>
                 </Link>
             </ThumbnailImage>
+            </span>
         )
 }
 
